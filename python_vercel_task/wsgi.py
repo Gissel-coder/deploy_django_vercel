047c0c0763e060c29925f4414d587e4eab9d4a69
Gissel-coder <gissel.lesme01@gmail.com>
2024-11-26 12:50:02 -0300

    Despliegue con Vercel

--- python_vercel_task/wsgi.py
+++ python_vercel_task/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_vercel_task.settings')
 
 application = get_wsgi_application()
+app= application
