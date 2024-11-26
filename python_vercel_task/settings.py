55a1ba9d08aa05fac208478e74f77a04276eeb58
Gissel-coder <gissel.lesme01@gmail.com>
2024-11-26 13:10:24 -0300

    Update settings.py

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','https://deploy-django-vercel-gilt.vercel.app/']
 
 
 # Application definition
